8d3bfc7c581e2210d02848d916c8f6520b2cf256
qa5011 <166572396+qa5011@users.noreply.github.com>
2025-09-03 07:32:01 +0700

    Add files via upload

---
+++ Book-Store/src/test/java/com/bookStore/Book/Store/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookStore.Book.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
